cd4e5b25fedc08b59843217a79c14178de8e8f2d
Hhaomu <qiaozhuobiao1996@qq.com>
2015-03-18 21:16:07 +0800

    helloworld

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	cout << "Hello,world!" << endl;
+	return 0;
+}
