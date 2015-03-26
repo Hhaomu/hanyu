22d0c173b9f7ac5e9fdb180d4c54e283ad5ed368
Hhaomu <qiaozhuobiao1996@qq.com>
2015-03-26 22:03:12 +0800

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std; 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main() {
-	cout << "Hello,world!" << endl;
-	return 0;
-}
